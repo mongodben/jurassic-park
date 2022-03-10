39c217507b202644c1e22966b03d881e9e09a2ac
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-03-10 10:40:57 -0500

    add inline role test

--- src/lib/parsers/snooty-to-pandoc-rst/replace-link-roles-with-links/index.ts
+++ src/lib/parsers/snooty-to-pandoc-rst/replace-link-roles-with-links/index.ts
@@ -1,15 +1,24 @@
-const LINK_ROLE_LIKE = /:([^\s]+):`(.*)<(.*)>`/g; // like: :smthn:`text blah blah <text-no-spaces>`
+const LINK_ROLE_LIKE = /:(.*):`([^<`]*)(<(.*)>)?`/g;
+// like: :smthn:`text blah blah <text-no-spaces>`
+// or 2 lines:
+// :smthn:`text blah blah
+// <text-no-spaces>`
 
-function replaceRoles(input: string, roles: dictionary) {
+function replaceRoles(
+  input: string,
+  roles: dictionary,
+  rolesToIgnore: string[] = ['ref', 'term'],
+) {
   const res = input.replaceAll(
     LINK_ROLE_LIKE,
-    (_match, linkRole, linkText, relativeUrl) => {
+    (match, linkRole, linkText, _, relativeUrl) => {
+      if (rolesToIgnore.includes(linkRole)) return match;
       if (!roles[linkRole]) {
         return `\`${linkText} _NO_MATCH_FOR_'${linkRole}'_ <${relativeUrl}>\`_`;
       }
       const baseUrl = roles[linkRole];
       const fullUrl = baseUrl.replace('%s', relativeUrl);
-      return `\`${linkText} <${fullUrl}>\`_`;
+      return `\`${linkText}<${fullUrl}>\`_`;
     },
   );
   return res;
---
+++ src/test/replace-link-roles-with-links.test.ts
@@ -0,0 +1,20 @@
+import replaceRolesWithLinks from '@/parsers/snooty-to-pandoc-rst/replace-link-roles-with-links';
+import { expect } from 'chai';
+
+const linksMap = {
+  wikipedia: 'https://en.wikipedia.org/wiki/%s',
+};
+
+const input1 = `:wikipedia:\`realm <realm>\``;
+const input2 = `:wikipedia:\`realm
+<realm>\``;
+
+it('replaces link roles with standard RST links', () => {
+  const res1 = replaceRolesWithLinks(input1, linksMap);
+  const res2 = replaceRolesWithLinks(input2, linksMap);
+
+  const expected1 = '`realm <https://en.wikipedia.org/wiki/realm>`_';
+  const expected2 = '`realm\n<https://en.wikipedia.org/wiki/realm>`_';
+  expect(res1).to.eql(expected1);
+  expect(res2).to.eql(expected2);
+});
