fc13d64bd2f81c60e6a092d50429f6ebb954fa1e
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-03-10 21:50:05 -0500

    update script

--- src/lib/parsers/pandoc-md-to-docusaurus-mdx/pandoc-md-admonitions-to-docusaurus.ts
+++ src/lib/parsers/pandoc-md-to-docusaurus-mdx/pandoc-md-admonitions-to-docusaurus.ts
@@ -1,17 +1,111 @@
-import { reporter } from 'vfile-reporter';
-import { remark } from 'remark';
-import remarkPresetLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import fs from 'fs';
+import { frontmatter } from 'micromark-extension-frontmatter';
+import {
+  frontmatterFromMarkdown,
+  frontmatterToMarkdown,
+} from 'mdast-util-frontmatter';
+import { gfm } from 'micromark-extension-gfm';
+import { gfmFromMarkdown, gfmToMarkdown } from 'mdast-util-gfm';
+import { mdxjs } from 'micromark-extension-mdxjs';
+import { mdxFromMarkdown, mdxToMarkdown } from 'mdast-util-mdx';
+import { toMarkdown } from 'mdast-util-to-markdown';
+
+const doc = `
+import Burp from './Burp.js';
+
+
+# Derppp
+
+### Merp
+
+:::note Meep
+
+**bold**
+
+hello world
+
+:::
+
+<NameOfInclude/>
+
+<Burp id="swag"/>
+
+<div class="not-an-include">
+
+/name/of/include.rst
+
+</div>
+`;
 
 async function main(): Promise<void> {
-  const res = await remark()
-    .use(remarkPresetLintMarkdownStyleGuide)
-    .process('*emphasis* and _importance_');
-  console.log(String(res));
-  console.log('derp', reporter(res));
-  // .then((file) => {
-  //   console.log(String(file));
-  //   console.error(reporter(file));
-  // });
+  const tree = fromMarkdown(doc, {
+    extensions: [frontmatter(['yaml']), gfm(), mdxjs()],
+    mdastExtensions: [
+      frontmatterFromMarkdown(['yaml']),
+      gfmFromMarkdown(),
+      mdxFromMarkdown(),
+    ],
+  });
+  // console.log(tree);
+
+  fs.writeFileSync('treeout.json', JSON.stringify(tree, null, 2));
+
+  traverseNodes(tree, tree);
+  const docOut = toMarkdown(tree, {
+    extensions: [
+      frontmatterToMarkdown(['yaml']),
+      gfmToMarkdown(),
+      mdxToMarkdown(),
+    ],
+  });
+  fs.writeFileSync('docout.md', docOut);
+  // const res = await unified()
+  //   .use(remarkParse)
+  //   .use(remarkFrontmatter)
+  //   .use(remarkGfm);
+  // // .process('---\nlayout: home\n---\n\n# Hi ~~Mars~~Venus!');
+  // console.log(String(res));
+  // console.log('derp', reporter(res));
+  // // .then((file) => {
+  // //   console.log(String(file));
+  // //   console.error(reporter(file));
+  // // });
+}
+
+function traverseNodes(node, root) {
+  createTitleFrontMatter(node);
+  convertIncludesToMdx(node, root, 'not-an-include');
+  if (!node.children) return;
+  node.children.forEach((child) => traverseNodes(child, root));
+}
+
+function createTitleFrontMatter(node) {
+  if (node.type === 'root') {
+    for (let i = 0; i < node.children.length; i++) {
+      const child = node.children[i];
+      if (child.type === 'heading' && child.depth === 1) {
+        const title = child.children[0].value;
+        const titleYaml = `title: ${title}`;
+        console.log(title);
+        const firstNode = node.children[0];
+        if (firstNode.type === 'yaml') {
+          firstNode.value += '\n' + titleYaml;
+          break;
+        } else {
+          node.children.unshift({
+            type: 'yaml',
+            value: titleYaml,
+          });
+          break;
+        }
+      }
+    }
+  }
+}
+
+function convertIncludesToMdx(node, root, className: string){
+  if(node.cl)
 }
 
 export default main;
